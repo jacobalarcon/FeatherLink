0df35846ca4ea09c4127d40a5c7bfeadc4c22268
Jacob Alarcon <jacobalarcon@gmail.com>
2023-04-05 20:21:03 -0500

    initial setup

--- server/broker.js
+++ server/broker.js
@@ -1,3 +1,17 @@
+const { Socket } = require('dgram');
 const express = require('express');
 const app = express();
 const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  socket.on('publisher_message', (message) => {
+    console.log(message);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on 3000');
+});
